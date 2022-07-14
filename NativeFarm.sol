899f3bf0ab17d036c63ffb88785ad1fdb70562af
RetroDefi <retrofinancebsc@gmail.com>
2022-07-14 11:46:26 -0500

    Update NativeFarm.sol

--- NativeFarm.sol
+++ NativeFarm.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.7 <0.9.0;
+pragma solidity >=0.8.7;
 
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 abstract contract Context {
