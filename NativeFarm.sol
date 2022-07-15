7d40ca94d8ab45b42bcb0188f1bacf5831b3fd5a
RetroDefi <retrofinancebsc@gmail.com>
2022-07-15 07:21:12 -0500

    Update NativeFarm.sol

--- NativeFarm.sol
+++ NativeFarm.sol
@@ -74,7 +74,7 @@ library Address {
             address(this).balance >= value,
             "Address: insufficient balance for call"
         );
-        //slither-disable-next-line arbitrary-send
+        //slither-disable-next-line low-level-calls
         (bool success, bytes memory returndata) = target.call{value: value}(
             data
         );
